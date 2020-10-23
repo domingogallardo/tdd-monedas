f47e6e1ccfdfa0ea1cdcc8581342a9809f72fdf8
Domingo Gallardo <domingo.gallardo@ua.es>
2020-10-23 12:05:25 +0200

    Refactorización usar monedas (5)

--- src/TestMonedas.java
+++ src/TestMonedas.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class TestMonedas {
     @Test
     public void testCrearMoneda() {
-        assertEquals(new Dolar(5), new Dolar(5));
+        assertEquals(Moneda.dolar(5), Moneda.dolar(5));
     }
 
     @Test
@@ -17,11 +17,11 @@ public class TestMonedas {
 
     @Test
     public void testEquals() {
-        assertTrue(new Dolar(5).equals(new Dolar(5)));
-        assertFalse(new Dolar(5).equals(new Dolar(6)));
-        assertTrue(new Euro(5).equals(new Euro(5)));
-        assertFalse(new Euro(5).equals(new Euro(6)));
-        assertFalse(new Euro(5).equals(new Dolar(5)));
+        assertTrue(Moneda.dolar(5).equals(Moneda.dolar(5)));
+        assertFalse(Moneda.dolar(5).equals(Moneda.dolar(6)));
+        assertTrue(Moneda.euro(5).equals(Moneda.euro(5)));
+        assertFalse(Moneda.euro(5).equals(Moneda.euro(6)));
+        assertFalse(Moneda.euro(5).equals(Moneda.dolar(5)));
     }
 
     @Test

2c1d2c4fe1e9c84f5134ddca8bc86540e32dc55a
Domingo Gallardo <domingo.gallardo@ua.es>
2020-10-23 13:50:09 +0200

    Refactorización eliminación Dolar y Euro (2)

--- src/Dolar.java
+++
@@ -1,6 +0,0 @@
-public class Dolar extends Moneda {
-
-    public Dolar(int cantidad, String denominacion) {
-        super(cantidad, denominacion);
-    }
-}
--- src/Euro.java
+++
@@ -1,6 +0,0 @@
-public class Euro extends Moneda {
-
-    public Euro(int cantidad, String denominacion) {
-        super(cantidad, denominacion);
-    }
-}
